909c92552e7e4b2764298a32183701118da7a80f
Sergio Ortiz <65801936+SergioNAO@users.noreply.github.com>
2023-07-31 11:30:41 -0600

    Add files via upload

---
+++ Numero Par o Impar/Num_Par_o_Impar/Num_Par_o_Impar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Num_Par_o_Impar
+{
+    class program { 
+    static void Main(string[] args)
+    {
+        Console.Write("Ingresa un número entero: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"El número {numero} es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numero} es impar.");
+        }
+            Console.ReadKey();
+    }
+    }
+}
