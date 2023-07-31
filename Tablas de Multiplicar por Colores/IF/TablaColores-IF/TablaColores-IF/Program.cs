8cc17081d11eaa3906c07303bb57e7cf9e1841c6
Sergio Ortiz <65801936+SergioNAO@users.noreply.github.com>
2023-07-31 11:31:34 -0600

    Add files via upload

---
+++ Tablas de Multiplicar por Colores/IF/TablaColores-IF/TablaColores-IF/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TablaColores_IF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un color (Amarillo, Morado, Rosa, Azul o Blanco): ");
+            string color = Console.ReadLine().ToLower();
+
+            if (color == "amarillo")
+            {
+                MostrarTablaMultiplicar(2);
+            }
+            else if (color == "morado")
+            {
+                MostrarTablaMultiplicar(7);
+            }
+            else if (color == "rosa")
+            {
+                MostrarTablaMultiplicar(9);
+            }
+            else if (color == "azul")
+            {
+                MostrarTablaMultiplicar(6);
+            }
+            else if (color == "blanco")
+            {
+                MostrarTablaMultiplicar(12);
+            }
+            else
+            {
+                Console.WriteLine("El color ingresado no es válido.");
+            }
+        }
+
+        static void MostrarTablaMultiplicar(int numero)
+        {
+            Console.WriteLine($"Tabla del {numero}:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
