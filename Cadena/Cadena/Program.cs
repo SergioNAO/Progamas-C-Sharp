aad7359f8befa22f110b84d72914275d41f259a2
Sergio Ortiz <65801936+SergioNAO@users.noreply.github.com>
2023-07-30 22:06:20 -0600

    Add files via upload

---
+++ Cadena/Cadena/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InversionCadenaTextoConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa el texto que deseas invertir: ");
+            string textoIngresado = Console.ReadLine();
+
+            char[] arregloCaracteres = textoIngresado.ToCharArray();
+            Array.Reverse(arregloCaracteres);
+
+            string resultadoInversion = new string(arregloCaracteres);
+
+            Console.WriteLine("Resultado de la inversión: " + resultadoInversion);
+        }
+    }
+}
